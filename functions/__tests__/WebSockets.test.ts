a2764c8ff238b71b62c2a3d73b03b67a434cf55c
JoeFazz <joseph@fazzino.net>
2019-04-12 22:27:05 +0100

    *shrug*

--- functions/__tests__/WebSockets.test.ts
+++ functions/__tests__/WebSockets.test.ts
@@ -1,13 +1,13 @@
 import { handleMessage } from '../websockets';
 import { MessageTypes } from '../../@types';
 
-describe('Test Clientside WebSockets', () => {
+const MOCK_STATE = {};
+
+describe('Test Client-Side WebSockets', () => {
     test('Message Receive', () => {
         const MOCK_EVENT: MessageEvent = new MessageEvent('test', {
             data: JSON.stringify({ type: MessageTypes.CONTAINER_RESUME, data: {} })
         });
-
-        const MOCK_STATE = {};
 
         expect(handleMessage(MOCK_EVENT, MOCK_STATE)).not.toBeUndefined();
     });
